6581ff4fba7a512d43631c8f0429ed01f9904e4b
Alberthluiiz <luisguillen290301@gmail.com>
2023-05-06 15:08:16 -0500

    Proyecto de integracion

---
+++ resources/views/auth/inicio.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('contenido')
+
+<div class="row justify-content-center gap-3 align-items-center p-5">
+    <div class="col-md-4 bg-light p-4 rounded shadow-lg">
+        <form>
+            @csrf
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input id="email" name="email" type="text" placeholder="Tu Email de Registro" class="form-control">
+            </div>
+
+            <div class="mb-3">
+                <label for="password" class="form-label">Password</label>
+                <input id="password" name="password" type="password" placeholder="Password de Registro" class="form-control">
+            </div>
+            <button type="submit" class="btn btn-primary w-100">Crear Cuenta</button>
+        </form>
+    </div>
+</div>
+
+@endsection
